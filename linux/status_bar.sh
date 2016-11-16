881e7f65cc30b003b4b4d8e65c0ab26736f6c7b1
Matthew Fleming <matthew.fleming@wanews.com.au>
2016-11-16 14:55:22 +0800

    Ultimate progress bar!

--- linux/status_bar.sh
+++ linux/status_bar.sh
@@ -3,23 +3,32 @@ function drawProgress {
     PERCENT=$1
     SIZE=$(($2))
     START=$3
-    PROGRESS=$((START+PERCENT*SIZE/100))
+    PROGRESS=$((PERCENT*SIZE/100))
     END=$((START+SIZE))
     if [ $PERCENT -eq 0 ]; then
         STRING=""
         for ((i=0; i<SIZE; i++)); do
             STRING+="░"
         done
-        echo -ne "\r\033[${START}C$STRING"
+        echo -ne "\r\033[${START}C$STRING    "
     elif [ $PROGRESS -lt $END ]; then
-        echo -ne "\r\033[${PROGRESS}C▓$PERCENT%"
+        STRING=""
+        for ((i=0; i<PROGRESS; i++)); do
+            STRING+="▓"
+        done
+        echo -ne "\r\033[${START}C$STRING$PERCENT%"
     else
         echo -ne "\r\033[${END}C100%"
     fi
 }
 
 echo -ne "Progress: "
-for i in {0..101}; do
+for i in {0..100}; do
     drawProgress $i 80 10
-    sleep 0.1
+    sleep 0.1 
 done
+for i in {0..10}; do
+    drawProgress $((10*i)) 80 10
+    sleep 1 
+done
+
