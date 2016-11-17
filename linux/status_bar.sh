a2a39fd6b7db1d71e99e3b7c5a69007b6b72cddc
Matthew Fleming <matthew.fleming@wanews.com.au>
2016-11-17 08:56:11 +0800

    Add status/progress

--- linux/status_bar.sh
+++ linux/status_bar.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 function drawProgress {
-    PERCENT=$1
-    SIZE=$(($2))
+    if [ $1 -gt 100 ]; then
+        PERCENT=100
+    elif [ $1 -lt 0 ]; then
+        PERCENT=0
+    else
+        PERCENT=$1
+    fi
+    SIZE=$2
     START=$3
     PROGRESS=$((PERCENT*SIZE/100))
     END=$((START+SIZE))
@@ -10,25 +16,54 @@ function drawProgress {
         for ((i=0; i<SIZE; i++)); do
             STRING+="░"
         done
-        echo -ne "\r\033[${START}C$STRING    "
-    elif [ $PROGRESS -lt $END ]; then
+        echo -ne "\r\033[${START}C$STRING\033[K\r\033[${START}C"
+    else
         STRING=""
         for ((i=0; i<PROGRESS; i++)); do
             STRING+="▓"
         done
         echo -ne "\r\033[${START}C$STRING$PERCENT%"
-    else
-        echo -ne "\r\033[${END}C100%"
     fi
 }
 
-echo -ne "Progress: "
-for i in {0..100}; do
-    drawProgress $i 80 10
-    sleep 0.1 
-done
-for i in {0..10}; do
-    drawProgress $((10*i)) 80 10
+function drawBorder {
+    if [ $2 == "top" ]; then
+        string="┌"
+    else
+        string="└"
+    fi
+    for ((i=2; i<$1; i++)); do
+        string+="─"
+    done
+    if [ $2 == "top" ]; then
+        string+="┐"
+    else
+        string+="┘"
+    fi
+    echo "$string"
+}
+
+function statusProgress {
+    width=$((`tput cols`))
+    proWidth=$((width-15))
+    if [ $1 -gt 0 ]; then
+        echo -ne "\033[1A\r"
+    fi
+    echo -e "Task:     ${@:2}\033[K"
+    echo -ne "Progress: "
+    drawProgress $1 $proWidth 10
+}
+
+tasks=(
+    'Reticulating Splines'
+    'Extracting Resources'
+    'Factoring Pay Scale'
+    'Lecturing Errant Subsystems'
+    'Mixing Genetic Pool'
+    'Complete'
+)
+for i in {0..5}; do
+    statusProgress $((22*i)) ${tasks[i]}
     sleep 1 
 done
 
