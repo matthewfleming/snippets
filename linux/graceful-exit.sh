03c97d8b4a66f7242789032df01db98fcc3113d6
Matthew Fleming <matt@DESKTOP-VGCTB3N.localdomain>
2017-08-28 16:02:26 +0800

    Improve graceful exit script

--- linux/graceful-exit.sh
+++ linux/graceful-exit.sh
@@ -2,11 +2,17 @@
 
 finally() {
     if [ "$killed" = true ]; then
-        wait
         echo "Processed $i files."
     else
         killed=true
+        wait
     fi
+}
+
+process() {
+    local file="$1"
+    echo "Processing $file"
+    curl www.google.com
 }
 
 # parse arguments
@@ -17,14 +23,23 @@ trap finally SIGINT SIGTERM EXIT
 
 i=0
 killed=false
+running=0
+threads=3
 
-while [ "$killed" = false -a "$i" -lt 10000 ]; do
+while [ "$i" -lt 10 ]; do
+    if [ "$running" -eq "$threads" ]; then
+        wait
+        running=0
+    fi
+    if [ "$killed" = true ]; then
+        break
+    else 
+
     let i++
-    echo "curl $i"
-    # will complete after ^C
-    coproc curl google.com -o "$i.txt" 
-    # will be interrupted after ^C
-    #curl google.com -o "$i.txt" 
-    wait 
-    echo "endcurl"
+    if [ "$i" -lt "$skip" ]; then
+        logger "Skipping $i"
+    else 
+        process "$i" 1>&1 2>&2 &
+        let running++
+    fi
 done
