0c72032fbf16c992115e12a0edab1f370ce28bc3
Matthew Fleming <matthew.fleming@wanews.com.au>
2017-10-19 10:01:52 +0800

    Fix ObjectToArray

--- php/ObjectToArray.php
+++ php/ObjectToArray.php
@@ -3,16 +3,28 @@
 class ObjectToArray
 {
 
-    public static function objectToArray(&$object)
+    protected $objectHashes = [];
+
+    public static function convert(&$object)
+    {
+        $converter = new ObjectToArray();
+        return $converter->objectToArray($object);
+    }
+
+    protected function convertObjectToArray(&$object)
     {
         if (is_object($object)) {
             // prevent infinite recursion for self or circulat references
             $hash = spl_object_hash($object);
-            if (in_array($hash, $this->objects)) {
+            if (in_array($hash, $this->objectHashes)) {
                 return '{...}';
             }
-            $this->objects[] = $hash;
+            $this->objectHashes[] = $hash;
             $clone = (array) $object;
+            // Prevent deeply nested trace
+            if ($object instanceof \Exception) {
+                $clone['trace'] = $object->getTraceAsString();
+            }
         } else if (is_array($object)) {
             $clone = &$object;
         } else {
