74f403e203e9111cf8b76210ce3eab64d4ca9ca7
Matthew Fleming <matthew.fleming@wanews.com.au>
2018-05-04 11:35:25 +0800

    Add lxss bootstrap script

---
+++ lxss/bootstrap.sh
@@ -0,0 +1,6 @@
+sudo apt install -y python-software-properties
+sudo add-apt-repository -y ppa:ondrej/php
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install php7.1
+yum install php7.1-common php7.1-fpm php7.1-opcache php7.1-cli php7.1-ldap php7.1-xml php7.1-soap php7.1-mysqlnd php7.1-pdo php7.1-gd php7.1-mbstring php7.1-intl php7.1-posix -y
